7be396e9ad77ebb2693d6bf0eb951667a4eda5c4
Parker Nilson <parker.todd.nilson@gmail.com>
2024-12-14 20:59:09 -0700

    Format

--- src/lib/components/goals/EntryModal.svelte
+++ src/lib/components/goals/EntryModal.svelte
@@ -1,36 +1,42 @@
 <script lang="ts">
-	import { presenterFactory } from "$lib/factories";
-	import { Entry, Goal } from "$lib/model/domain/goals";
-	import { EntryGalleryPresenter } from "$lib/presenters/goals/EntryGalleryPresenter.svelte";
-	import { getContext } from "svelte";
-	import Modal from "../Modal.svelte";
+	import { presenterFactory } from '$lib/factories';
+	import { Entry, Goal } from '$lib/model/domain/goals';
+	import { EntryGalleryPresenter } from '$lib/presenters/goals/EntryGalleryPresenter.svelte';
+	import { getContext } from 'svelte';
+	import Modal from '../Modal.svelte';
 
-    let { entry, goal, showModal = $bindable() }: { entry: Entry | null, goal: Goal, showModal: boolean } = $props();
+	let {
+		entry,
+		goal,
+		showModal = $bindable()
+	}: { entry: Entry | null; goal: Goal; showModal: boolean } = $props();
 
-    const entryGalleryPresenter = getContext<EntryGalleryPresenter>("EntryGalleryPresenter");
-    const presenter = presenterFactory.createEntryModalPresenter(entryGalleryPresenter, goal, entry ?? undefined);
+	const entryGalleryPresenter = getContext<EntryGalleryPresenter>('EntryGalleryPresenter');
+	const presenter = presenterFactory.createEntryModalPresenter(
+		entryGalleryPresenter,
+		goal,
+		entry ?? undefined
+	);
 
-    async function updateEntry() {
-       await presenter.optimisticallyUpsertEntry(presenter.newEntry);
-        showModal = false;
-    }
-
+	async function updateEntry() {
+		await presenter.optimisticallyUpsertEntry(presenter.newEntry);
+		showModal = false;
+	}
 </script>
 
 <Modal bind:showModal>
-    {#snippet header()}<h1>{presenter.entry?.dateOf}</h1>{/snippet}
-    <div>
-        {#if presenter.isEditing}
-            <button onclick={presenter.cancelEditing.bind(presenter)}>Cancel</button>
-        {:else}
-            <button onclick={presenter.startEditing.bind(presenter)}>Edit</button>
-        {/if}
-    </div>
-    {#if presenter.isEditing}
-        <textarea bind:value={presenter.currentTextContent}></textarea>
-        <button class="block" onclick={updateEntry}>Update</button>
-    {:else}
-        <p>{presenter.entry?.textContent}</p>
-    {/if}
+	{#snippet header()}<h1>{presenter.entry?.dateOf}</h1>{/snippet}
+	<div>
+		{#if presenter.isEditing}
+			<button onclick={presenter.cancelEditing.bind(presenter)}>Cancel</button>
+		{:else}
+			<button onclick={presenter.startEditing.bind(presenter)}>Edit</button>
+		{/if}
+	</div>
+	{#if presenter.isEditing}
+		<textarea bind:value={presenter.currentTextContent}></textarea>
+		<button class="block" onclick={updateEntry}>Update</button>
+	{:else}
+		<p>{presenter.entry?.textContent}</p>
+	{/if}
 </Modal>
-
