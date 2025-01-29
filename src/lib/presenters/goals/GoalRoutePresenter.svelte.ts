f567a082adcb51f45c38c58f44cdfd8f0ef9b499
Parker Nilson <parker.todd.nilson@gmail.com>
2025-01-29 14:26:50 -0700

    WIP: fix the goal route presenter load function

--- src/lib/presenters/goals/GoalRoutePresenter.svelte.ts
+++ src/lib/presenters/goals/GoalRoutePresenter.svelte.ts
@@ -3,13 +3,13 @@ import type { ErrorService } from "$lib/services/ErrorService.svelte";
 import { LoadablePresenter } from "../LoadablePresenter.svelte";
 
 export class GoalRoutePresenter extends LoadablePresenter {
-    get goal() { return this.goalModel.goal }
+    get goal() { return this.goalModel.data }
 
 	constructor(errorService: ErrorService, private goalModel: GoalModel) {
         super(errorService);
     }
 
     protected async loadResource(): Promise<void> {
-        await this.goalModel.loadGoal();
+        await this.goalModel.load();
     }
 }
