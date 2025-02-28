95347d34ba8fd2e397a84599ff1e0634c0c50925
Parker Nilson <parker.todd.nilson@gmail.com>
2025-02-28 15:03:00 -0700

    Fix the checkmark

--- src/lib/components/goals/EntrySquare.svelte
+++ src/lib/components/goals/EntrySquare.svelte
@@ -26,14 +26,18 @@
 	<p class="self-end">{presenter.dateFormatted}</p>
 	{#if presenter.entry.textContent}
 		{@const len = presenter.entry.textContent.length}
-		<div class="flex-1 flex flex-col justify-between">
-			<span></span>
-			<p class={len > 20 ? 'text-lg' : 'text-xl'}>{presenter.entry.textContent}</p>
-			<div class="w-6 self-end">
-				{#if presenter.entry.success}
-					<CircleCheck />
-				{/if}
-			</div>
+		<div class="flex-1 flex items-center pb-6">
+			<!-- TODO: make the text change size based on length (calculate based on max characters) -->
+			 <!-- and truncate the text based on max character length (take emojis into account) -->
+			<p class="
+			 	{len < 10 ? 'text-lg' : len < 20 ? 'text-md' : 'text-sm'}
+				sm:{len < 10 ? 'text-xl' : len < 20 ? 'text-lg' : 'text-md'}
+			">{presenter.entry.textContent}</p>
+		</div>
+	{/if}
+	{#if presenter.entry.success}
+		<div class="w-4 xs:w-6 absolute bottom-3 right-3">
+			<CircleCheck />
 		</div>
 	{/if}
 </div>
