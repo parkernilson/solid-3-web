e36a2beb36ebe97dfd3eff668c2ca44ed734cbb6
Parker Nilson <parker.todd.nilson@gmail.com>
2025-03-06 12:53:18 -0700

    Add log level and debug logger

---
+++ src/lib/utils/logging/log.ts
@@ -0,0 +1,13 @@
+import { PUBLIC_LOG_LEVEL } from '$env/static/public';
+
+type LogLevel = 'debug' | 'none';
+
+export const log =
+	(level: LogLevel) =>
+	(...args: unknown[]) => {
+		if (PUBLIC_LOG_LEVEL === level) {
+			console.log(...args);
+		}
+	};
+
+export const debug = log('debug');
